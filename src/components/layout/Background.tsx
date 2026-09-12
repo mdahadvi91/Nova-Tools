75e5be2311264de521c30cbf7b182475b83151b4
mdahadvi91 <mdahadvi91@gmail.com>
2026-09-12 04:04:40 +0400

    Refactor Background component structure
    Removed unnecessary fragment and overlay div from Background component.

--- src/components/layout/Background.tsx
+++ src/components/layout/Background.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
+limport React from 'react';
 
 const Background: React.FC = () => {
-return (
-<>
-<div
-aria-hidden="true"
-className="fixed inset-0 pointer-events-none z-0 bg-[#060b09] bg-cover bg-center bg-no-repeat"
-style={{
-backgroundImage: "url('/assets/background.jpg')",
-}}
-/>
-
-  <div
-    aria-hidden="true"
-    className="fixed inset-0 pointer-events-none z-0 app-bg-overlay"
-  />
-</>
-
-);
+  return (
+    <div
+      aria-hidden="true"
+      className="fixed inset-0 pointer-events-none z-0 bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: "url('/assets/background.jpg')",
+      }}
+    />
+  );
 };
 
 export default Background;
